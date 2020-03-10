c05528284972c7f719a09d1db443a186e67666da
Joe Paul <joe.paul0712@gmail.com>
2020-03-10 15:08:45 +0000

    using middleware

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const path = require('path');
+const moment = require('moment');
+const members = require('./Members');
 
 const app = express();
 
+const logger = (req, res, next) => {
+    console.log(
+        `${req.protocol}://${req.get('host')}${
+            req.originalUrl
+        }: ${moment().format()}`
+        );
+    next();
+};
+
+// Init middleware
+app.use(logger);
+
+// Gets all members
+app.get('/api/members', (req, res) => res.json(members));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = process.env.PORT || 5000;
 
-app.listen();
+app.listen(PORT, () => console.log(`Server started in port ${PORT}`));
